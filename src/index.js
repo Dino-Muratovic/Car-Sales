484c3129cacab96b4c311a768be8f0a250aced95
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-04-14 18:45:16 -0500

    First push to my branch, created few components and connected them together

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import reducer from './reducers/reducer';
+import { Provider } from 'react-redux'; // bridge piece
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+
+
+
+//Set up REDUX store
+
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+
+<Provider store={store}>
+    <App />
+</Provider>, rootElement);
